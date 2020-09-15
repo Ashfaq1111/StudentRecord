a312ed2ee2c95f91c020fd5c3fdad33b75ec5160
Safi Ullah <safi90655@gmail.com>
2020-09-15 23:59:02 +0500

    Add project files.

---
+++ StudentRecord/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace StudentRecord.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string FatherName { get; set; }
+        public string Class { get; set; }
+        public int marks { get; set; }
+
+    }
+
+    public class StudentDBContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
